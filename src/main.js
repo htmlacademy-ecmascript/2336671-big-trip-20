8db567e8637949c1338541b938a5b73936c96243
yesiammark <yesiammark@gmail.com>
2023-05-02 16:33:54 +0300

    Правит именование классов

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import filtersView from './View/filters-view.js';
-import sortingView from './View/sorting-view.js';
+import FiltersView from './View/filters-view.js';
+import SortingView from './View/sorting-view.js';
 import {render} from './render.js';
 
 const filtersControls = document.querySelector('.trip-controls__filters');
 const sortingControls = document.querySelector('.trip-events');
 
-render (new filtersView(), filtersControls);
-render (new sortingView(), sortingControls);
+render (new FiltersView(), filtersControls);
+render (new SortingView(), sortingControls);
