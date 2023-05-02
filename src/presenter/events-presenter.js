43f095a17a041585f2e271b5c119aae8659e29fd
yesiammark <yesiammark@gmail.com>
2023-05-02 18:28:04 +0300

    Правит именование директории

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,7 +1,7 @@
-import TripEventsListView from '../view/trip-events_list.js';
-import TripEventsItemView from '../view/trip-events_item.js';
-import EditPointView from '../view/edit-point-view.js';
-import SortingView from '../view/sorting-view.js';
+import TripEventsListView from '../View/trip-events_list.js';
+import TripEventsItemView from '../View/trip-events_item.js';
+import EditPointView from '../View/edit-point-view.js';
+import SortingView from '../View/sorting-view.js';
 import { render } from '../render.js';
 
 export default class EventsPresenter {
