646296b4e566257bc9d438c223711a5bd1587ce9
yesiammark <yesiammark@gmail.com>
2023-05-02 18:03:55 +0300

    Добавляет список ивентов и редактирование

---
+++ src/View/trip-events_list.js
@@ -0,0 +1,25 @@
+//trip-events__list
+
+import {createElement} from '../render.js';
+
+function cretateTripEventsListTemplate () {
+  return '<ul class="trip-events__list"></ul>';
+}
+
+export default class TripEventsListView {
+  getTemplate () {
+    return cretateTripEventsListTemplate();
+  }
+
+  getElement () {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement () {
+    this.element = null;
+  }
+}
