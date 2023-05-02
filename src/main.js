4ad6024ce1395fbb893335d0b0606441d8dd848f
yesiammark <yesiammark@gmail.com>
2023-05-02 16:10:58 +0300

    Добавляет сортировку

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import filtersView from './View/filters-view.js';
+import sortingView from './View/sorting-view.js';
 import {render} from './render.js';
 
 const filtersControls = document.querySelector('.trip-controls__filters');
+const sortingControls = document.querySelector('.trip-events');
 
 render (new filtersView(), filtersControls);
+render (new sortingView(), sortingControls);
