81441ce421c4974231e6d6b2be8e1ecbe6125f56
yesiammark <yesiammark@gmail.com>
2023-05-02 17:01:22 +0300

    добавляет trip view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import {RenderPosition, render} from './render.js';
 import FiltersView from './View/filters-view.js';
 import SortingView from './View/sorting-view.js';
-import {render} from './render.js';
+import TripInfoView from './View/trip-info-view.js';
 
+const tripMainElement = document.querySelector('.trip-main');
 const filtersControls = document.querySelector('.trip-controls__filters');
 const sortingControls = document.querySelector('.trip-events');
 
+render (new TripInfoView(),tripMainElement, RenderPosition.AFTERBEGIN);
 render (new FiltersView(), filtersControls);
 render (new SortingView(), sortingControls);
+
