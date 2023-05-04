e3d678a39dd161e8b1fcb756393dd4ec074b3169
yesiammark <yesiammark@gmail.com>
2023-05-04 16:16:31 +0300

    Добавляет презентер инфо о путешествии

---
+++ src/presenter/trip-info-presenter.js
@@ -0,0 +1,18 @@
+import { RenderPosition } from '../render.js';
+import { render } from '../render.js';
+import TripInfoView from '../View/trip-info-view.js';
+
+export default class TripInfoPresenter {
+
+  constructor({eventContainer, pointsModel}) {
+    this.eventContainer = eventContainer;
+    this.pointsModel = pointsModel;
+  }
+
+  init () {
+    this.eventPoints = [...this.pointsModel.getPoints()];
+
+    render(new TripInfoView({points: this.eventPoints}), this.eventContainer, RenderPosition.AFTERBEGIN);
+
+  }
+}
