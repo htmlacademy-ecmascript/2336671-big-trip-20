0fd63798c017ddd732c97c567795346f11c063bf
yesiammark <yesiammark@gmail.com>
2023-06-01 20:14:05 +0300

    Правит модель: - Добавление точки - Удаление точки - Обновление точки

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -9,4 +9,31 @@ export default class PointsModel extends Observable {
   get points() {
     return this.#points;
   }
+
+  addPoint (updateType, update) {
+    this.#points = [update, ...this.points];
+
+    this._notify(updateType, update);
+  }
+
+  deletePoint (updateType, update) {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+
+    if (index > -1) {
+      this.#points.splice(index, 1);
+    }
+
+    this._notify(updateType, update);
+  }
+
+  updatePoint (updateType, update) {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+
+    if(index > -1) {
+      this.#points[index] = update;
+    }
+
+    this._notify(updateType, update);
+  }
+
 }
